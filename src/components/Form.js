5105872417b23d585c86defd9a7812a6defb7183
Nathan Leung <18nleung@gmail.com>
2016-08-28 11:21:29 +0000

    fiddling with forex select

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 // react-select css is loaded in the index.jade file
 import Select from 'react-select';
+import { isEmpty } from 'lodash';
 import { BTC, USD } from '../constants/CurrencyCodes';
 import InputContainer from '../containers/InputContainer';
 import styles from '../styles/Form.css';
@@ -11,17 +12,23 @@ const Form = ({ data, forexData }) => {
     value: BTC,
     label: 'BTC',
   }];
-  const fiatOptions = Object.keys(forexData).map((curr) => ({
+  let fiatOptions = Object.keys(forexData).map((curr) => ({
     value: curr,
     label: curr,
   }));
+  if (isEmpty(fiatOptions)) {
+    fiatOptions = [{
+      value: USD,
+      label: 'USD',
+    }];
+  }
   return (
     <form className={styles.form}>
       <InputContainer currency={BTC} data={data} />
       <Select className={styles.select} value={BTC} options={cryptoOptions} />
       {' = '}
       <InputContainer currency={USD} data={data} />
-      <Select className={styles.select} options={fiatOptions} />
+      <Select className={styles.select} value={USD} options={fiatOptions} />
     </form>
   );
 };
